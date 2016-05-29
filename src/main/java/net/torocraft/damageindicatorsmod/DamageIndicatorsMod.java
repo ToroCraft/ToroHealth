731e22a7b2025a6e20358eb1ce86ca6c7bf89a14
Zane Riley <zeriley@gmail.com>
2016-05-29 11:13:35 -0400

    stepping version to 1.9.4-3

--- src/main/java/net/torocraft/damageindicatorsmod/DamageIndicatorsMod.java
+++ src/main/java/net/torocraft/damageindicatorsmod/DamageIndicatorsMod.java
@@ -14,7 +14,7 @@ import net.torocraft.damageindicatorsmod.events.Events;
 public class DamageIndicatorsMod {
 
 	public static final String MODID = "damageindicatorsmod";
-	public static final String VERSION = "1.9.4-1";
+	public static final String VERSION = "1.9.4-3";
 	public static final String MODNAME = "DamageIndicatorsMod";
 
 	@SidedProxy(clientSide = "net.torocraft.damageindicatorsmod.ClientProxy", serverSide = "net.torocraft.damageindicatorsmod.ServerProxy")
