7bd1620aaf93a11679afe1e8dc54146ba4b49388
Charles Howard <frodare@gmail.com>
2020-12-20 11:01:48 -0500

    Updated damage rounding #73

--- src/main/java/net/torocraft/torohealth/bars/BarState.java
+++ src/main/java/net/torocraft/torohealth/bars/BarState.java
@@ -1,16 +1,19 @@
 package net.torocraft.torohealth.bars;
 
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.util.math.MathHelper;
 import net.torocraft.torohealth.ToroHealth;
 
 public class BarState {
 
   public final LivingEntity entity;
 
+  public float health;
   public float previousHealth;
   public float previousHealthDisplay;
   public float previousHealthDelay;
-  public float lastDmg;
+  public int lastDmg;
+  public int lastDmgCumulative;
   public float lastHealth;
   public float lastDmgDelay;
   private float animationSpeed = 0;
@@ -22,39 +25,61 @@ public class BarState {
   }
 
   public void tick() {
+    health = entity.getHealth();
+    incrementTimers();
+
+    if (lastHealth < 0.1) {
+      reset();
+
+    } else if (lastHealth != health) {
+      handleHealthChange();
+
+    } else if (lastDmgDelay == 0.0F) {
+      reset();
+    }
+
+    updateAnimations();
+  }
+
+  private void reset() {
+    lastHealth = health;
+    lastDmg = 0;
+    lastDmgCumulative = 0;
+  }
+
+  private void incrementTimers() {
     if (this.lastDmgDelay > 0) {
       this.lastDmgDelay--;
     }
     if (this.previousHealthDelay > 0) {
       this.previousHealthDelay--;
     }
+  }
 
-    if (lastHealth < 0.1) {
-      lastHealth = entity.getHealth();
-      lastDmg = 0;
-    } else if (lastHealth != entity.getHealth()) {
-      lastDmg = lastHealth - entity.getHealth();
-      lastDmgDelay = HEALTH_INDICATOR_DELAY * 2;
-      lastHealth = entity.getHealth();
-      if (ToroHealth.CONFIG.particle.show) {
-        BarStates.PARTICLES.add(new BarParticle(entity, lastDmg));
-      }
-    } else if (lastDmgDelay == 0.0F) {
-      lastHealth = entity.getHealth();
-      lastDmg = 0;
+  private void handleHealthChange() {
+    lastDmg = MathHelper.ceil(lastHealth) - MathHelper.ceil(health);
+    lastDmgCumulative += lastDmg;
+
+    lastDmgDelay = HEALTH_INDICATOR_DELAY * 2;
+    lastHealth = health;
+    if (ToroHealth.CONFIG.particle.show) {
+      BarStates.PARTICLES.add(new BarParticle(entity, lastDmg));
     }
+  }
 
+  private void updateAnimations() {
     if (previousHealthDelay > 0) {
-      float diff = previousHealthDisplay - entity.getHealth();
+      float diff = previousHealthDisplay - health;
       if (diff > 0) {
         animationSpeed = diff / 10f;
       }
-    } else if (previousHealthDelay < 1 && previousHealthDisplay > entity.getHealth()) {
+    } else if (previousHealthDelay < 1 && previousHealthDisplay > health) {
       previousHealthDisplay -= animationSpeed;
     } else {
-      previousHealthDisplay = entity.getHealth();
-      previousHealth = entity.getHealth();
+      previousHealthDisplay = health;
+      previousHealth = health;
       previousHealthDelay = HEALTH_INDICATOR_DELAY;
     }
   }
+
 }
