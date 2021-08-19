014142718868c6a6288af1d4d27fd42d87673355
unascribed <me@unascribed.com>
2021-08-18 20:13:15 -0700

    Fix color parsing using java.awt.Color (#120)

--- src/main/java/net/torocraft/torohealth/config/loader/ColorJsonAdpater.java
+++ src/main/java/net/torocraft/torohealth/config/loader/ColorJsonAdpater.java
@@ -1,31 +1,29 @@
 package net.torocraft.torohealth.config.loader;
 
+import com.google.common.base.CharMatcher;
 import com.google.gson.TypeAdapter;
 import com.google.gson.stream.JsonReader;
 import com.google.gson.stream.JsonWriter;
-import java.awt.Color;
 import java.io.IOException;
 
 public class ColorJsonAdpater extends TypeAdapter<Integer> {
 
+  private static final CharMatcher HEX = CharMatcher.anyOf("0123456789abcdefABCDEF");
+
   @Override
   public void write(JsonWriter out, Integer value) throws IOException {
-    String hex = Integer.toHexString(value & 0xffffff);
-    hex = String.format("#%1$6s", hex).replace(' ', '0');
+    String hex = String.format("#%06x", value & 0xFFFFFF);
     out.value(hex);
   }
 
   @Override
   public Integer read(JsonReader in) throws IOException {
     String read = in.nextString();
-    try {
-      Color c = Color.decode(read);
-      return c.getRGB();
-    } catch (Exception e) {
+    if (read.charAt(0) != '#' || read.length() != 7 || !HEX.matchesAllOf(read.substring(1))) {
       System.out.println("ToroHealth: failed to parse color [" + read + "]");
-      e.printStackTrace();
       return null;
     }
+    return Integer.parseInt(read.substring(1), 16);
   }
 
 }
