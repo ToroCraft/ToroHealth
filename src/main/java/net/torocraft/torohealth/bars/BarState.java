44a9955fa559a671f7444f96c4acd5f7974a1fd3
wbc3467-spec <wbc3467@gmail.com>
2025-12-04 09:28:27 +0000

    Fixed entity reloading error, issues/82, by changing the primiry var of barStatus to entity id instead of entity reference

--- src/main/java/net/torocraft/torohealth/bars/BarState.java
+++ src/main/java/net/torocraft/torohealth/bars/BarState.java
@@ -1,85 +1,106 @@
 package net.torocraft.torohealth.bars;
 
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.Entity;
 import net.minecraft.util.math.MathHelper;
+import net.minecraft.client.MinecraftClient;
 import net.torocraft.torohealth.ToroHealth;
 
 public class BarState {
 
-  public final LivingEntity entity;
+    public final Integer entityID;
 
-  public float health;
-  public float previousHealth;
-  public float previousHealthDisplay;
-  public float previousHealthDelay;
-  public int lastDmg;
-  public int lastDmgCumulative;
-  public float lastHealth;
-  public float lastDmgDelay;
-  private float animationSpeed = 0;
+    public float health;
+    public float previousHealthDisplay;
+    public float previousHealthDelay;
+    public int lastDmg;
+    public int lastDmgCumulative;
+    public float lastHealth;
+    public float lastDmgDelay;
+    private float animationSpeed;
 
-  private static final float HEALTH_INDICATOR_DELAY = 10;
+    private static final float HEALTH_INDICATOR_DELAY = 10;
 
-  public BarState(LivingEntity entity) {
-    this.entity = entity;
-  }
 
-  public void tick() {
-    health = Math.min(entity.getHealth(), entity.getMaxHealth());
-    incrementTimers();
 
-    if (lastHealth < 0.1) {
-      reset();
-
-    } else if (lastHealth != health) {
-      handleHealthChange();
-
-    } else if (lastDmgDelay == 0.0F) {
-      reset();
+    public BarState(Integer id) {
+        MinecraftClient client = MinecraftClient.getInstance();
+        Entity entity = client.world != null ? client.world.getEntityById(id) : null;
+        if (entity instanceof LivingEntity living) {
+            this.entityID = id;
+            health = Math.min(living.getHealth(), living.getMaxHealth());
+            previousHealthDisplay = health;
+            lastDmg = 0;
+            lastDmgCumulative = 0;
+            lastHealth = health;
+            lastDmgDelay = 0;
+            animationSpeed = 0;
+        } else {
+            this.entityID = null; // will be ignored
+        }
     }
 
-    updateAnimations();
-  }
+    public void tick() {
+        MinecraftClient client = MinecraftClient.getInstance();
+        LivingEntity entity = (LivingEntity) client.world.getEntityById(entityID);
 
-  private void reset() {
-    lastHealth = health;
-    lastDmg = 0;
-    lastDmgCumulative = 0;
-  }
+        if (entity != null){
+            health = Math.min(entity.getHealth(), entity.getMaxHealth());
+            incrementTimers();
 
-  private void incrementTimers() {
-    if (this.lastDmgDelay > 0) {
-      this.lastDmgDelay--;
+            if (lastHealth < 0.1) {
+                reset();
+
+            } else if (lastHealth != health) {
+                handleHealthChange();
+
+            } else if (lastDmgDelay == 0.0F) {
+                reset();
+            }
+
+            updateAnimations();
+        }
     }
-    if (this.previousHealthDelay > 0) {
-      this.previousHealthDelay--;
+
+    private void reset() {
+        lastHealth = health;
+        lastDmg = 0;
+        lastDmgCumulative = 0;
     }
-  }
 
-  private void handleHealthChange() {
-    lastDmg = MathHelper.ceil(lastHealth) - MathHelper.ceil(health);
-    lastDmgCumulative += lastDmg;
-
-    lastDmgDelay = HEALTH_INDICATOR_DELAY * 2;
-    lastHealth = health;
-    if (ToroHealth.CONFIG.particle.show) {
-      BarStates.PARTICLES.add(new BarParticle(entity, lastDmg));
+    private void incrementTimers() {
+        if (this.lastDmgDelay > 0) {
+            this.lastDmgDelay--;
+        }
+        if (this.previousHealthDelay > 0) {
+            this.previousHealthDelay--;
+        }
     }
-  }
 
-  private void updateAnimations() {
-    if (previousHealthDelay > 0) {
-      float diff = previousHealthDisplay - health;
-      if (diff > 0) {
-        animationSpeed = diff / 10f;
-      }
-    } else if (previousHealthDelay < 1 && previousHealthDisplay > health) {
-      previousHealthDisplay -= animationSpeed;
-    } else {
-      previousHealthDisplay = health;
-      previousHealth = health;
-      previousHealthDelay = HEALTH_INDICATOR_DELAY;
+    private void handleHealthChange() {
+        lastDmg = MathHelper.ceil(lastHealth) - MathHelper.ceil(health);
+        lastDmgCumulative += lastDmg;
+
+        lastDmgDelay = HEALTH_INDICATOR_DELAY * 2;
+        lastHealth = health;
+        if (ToroHealth.CONFIG.particle.show) {
+            MinecraftClient client = MinecraftClient.getInstance();
+            LivingEntity entity = (LivingEntity) client.world.getEntityById(entityID);
+            BarStates.PARTICLES.add(new BarParticle(entity, lastDmg));
+        }
     }
-  }
 
+    private void updateAnimations() {
+        if (previousHealthDelay > 0) {
+            float diff = previousHealthDisplay - health;
+            if (diff > 0) {
+                animationSpeed = diff / 10f;
+            }
+        } else if (previousHealthDelay < 1 && previousHealthDisplay > health) {
+            previousHealthDisplay -= animationSpeed;
+        } else {
+            previousHealthDisplay = health;
+            previousHealthDelay = HEALTH_INDICATOR_DELAY;
+        }
+    }
 }
--- src/main/java/net/torocraft/torohealth/bars/BarStates.java
+++ src/main/java/net/torocraft/torohealth/bars/BarStates.java
@@ -18,7 +18,7 @@ public class BarStates {
     int id = entity.getId();
     BarState state = STATES.get(id);
     if (state == null) {
-      state = new BarState(entity);
+      state = new BarState(id);
       STATES.put(id, state);
     }
     return state;
